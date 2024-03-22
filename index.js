616b6cb94231bc94a8c57e7ba26f3db74a55990b
Stan van Rooy <me@stanvanrooy.com>
2024-03-22 13:06:21 +0100

    feat; update

--- index.js
+++ index.js
@@ -1,58 +1,32 @@
-/**
- * Options for initializing the Elfsquad Showroom.
- */
 export interface ElfsquadShowroomOptions {
   /**
-   * The container to render the iframe element in. Accepts either a HTMLElement or a query selector string.
-   */
+   * The container to render the iframe element in. Accepts a HTMLElement or a query selector.
+   **/
   container: HTMLElement | string;
 
   /**
-   * The URL of the Elfsquad showroom, which could be a general showroom URL or a specific product URL.
-   */
+   * The URL of the Elfsquad showroom, either to the showroom itself or to a specific product.
+  **/
   url: string;
 }
 
-/**
- * Type definition for the screenshot callback function.
- */
-export type ScreenshotCallback = (data: { image: string }) => void;
 
-/**
- * Type definition for the request quote callback function.
- */
+export type ScreenshotCallback = (data: { image: string }) => void;
 export type RequestQuoteCallback = (data: { configurationId: string }) => void;
 
-/**
- * Enum representing different viewer events within the Elfsquad Showroom.
- */
+
 export enum ViewerEvent {
   ScreenshotTaken = 'elfsquad.visualization.screenshotTaken',
   RequestQuote = 'custom:REQUEST_QUOTE',
 }
 
-/**
- * Class representing an Elfsquad Showroom instance.
- */
+
 export class ElfsquadShowroom {
   private readonly nativeElement: HTMLIFrameElement;
   private container: HTMLElement;
 
   private readonly callbacks: { [key: string]: ((data: any) => void)[] } = {};
 
-  /**
-   * Initializes a new instance of the Elfsquad Showroom.
-   *
-   * @example
-   * ```typescript
-   * const showroom = new ElfsquadShowroom({ container: '#showroom', url: 'https://automotive.elfsquad.io' });
-   * ```
-   *
-   * @param options - The options used to initialize the showroom.
-   * @throws Will throw an error if the specified container is not found or is not an HTMLElement.
-   * @throws Will throw an error if the native iframe element does not have a content window.
-   * @returns A new instance of the Elfsquad Showroom.
-   */
   constructor(options: ElfsquadShowroomOptions) {
     this.container = this.getContainer(options);
     this.nativeElement = this.render(options);
@@ -61,89 +35,51 @@ export class ElfsquadShowroom {
   }
 
   /**
-   * Retrieves the native iframe element.
-   *
-   * @example
-   * ```typescript
-   * const showroom = new ElfsquadShowroom({ container: '#showroom', url: 'https://automotive.elfsquad.io' });
-   * const iframe = showroom.getNativeElement();
-   * ```
-   * 
-   * @returns The native iframe element.
-   */
+   * Get the native iframe element.
+   * @returns {HTMLIFrameElement} the native iframe element.
+   * */
   public getNativeElement(): HTMLIFrameElement {
     return this.nativeElement;
   }
 
   /**
    * Resets the viewer to the home camera position.
-   *
-   * @example
-   * ```typescript
-   * const showroom = new ElfsquadShowroom({ container: '#showroom', url: 'https://automotive.elfsquad.io' });
-   * showroom.home();
-   * ```
-   */
+   * @returns {void}
+   * */
   public home(): void {
     this.sendMessage({ name: 'elfsquad.visualization.home' });
   }
 
   /**
    * Toggles the visibility of the footprint.
-   *
-   * @example
-   * ```typescript
-   * const showroom = new ElfsquadShowroom({ container: '#showroom', url: 'https://automotive.elfsquad.io' });
-   * showroom.toggleFootprint();
-   * ```
-   */
+   * @returns {void}
+   * */
   public toggleFootprint(): void {
     this.sendMessage({ name: 'elfsquad.visualization.toggleFootprint' });
   }
 
   /**
-   * Initiates a screenshot of the current view. The screenshot data is returned via the `onScreenshot` callback.
-   *
-   * @example
-   * ```typescript
-   * const showroom = new ElfsquadShowroom({ container: '#showroom', url: 'https://automotive.elfsquad.io' });
-   * showroom.screenshot();
-   * ```
-   */
+   * Trigger a screenshot of the current view. The screenshot will be returned in the onScreenshot callback.
+   * @returns {void}
+   * */
   public screenshot(): void {
     this.sendMessage({ name: 'elfsquad.visualization.screenshot' });
   }
 
   /**
-   * Registers a callback function to be invoked when a screenshot is taken.
-   *
-   * @example
-   * ```typescript
-   * const showroom = new ElfsquadShowroom({ container: '#showroom', url: 'https://automotive.elfsquad.io' });
-   * showroom.onScreenshot(data => {
-   *  console.log(data.image);
-   * });
-   * ```
-   *
-   * @param callback - The callback function to be called upon screenshot capture.
-   */
+   * Register a callback function to be called when a screenshot is taken.
+   * @param {(data: { image: string })} callback - The callback function to be called when a screenshot is taken.
+   * @returns {void}
+   * */
   public onScreenshot(callback: ScreenshotCallback): void {
     this.addCallback(ViewerEvent.ScreenshotTaken, callback);
   }
 
   /**
-   * Registers a callback function to be invoked when a quote is requested.
-   *
-   * @example
-   * ```typescript
-   * const showroom = new ElfsquadShowroom({ container: '#showroom', url: 'https://automotive.elfsquad.io' });
-   * showroom.onRequestQuote(data => {
-   *  console.log(data.configurationId);
-   * });
-   * ```
-   *
-   * @param callback - The callback function to be called upon quote request.
-   */
+   * Register a callback function to be called when a quote is requested.
+   * @param {(data: { configurationId: string })} callback - The callback function to be called when a quote is requested.
+   * @returns {void}
+   * */
   public onRequestQuote(callback: RequestQuoteCallback): void {
     this.addCallback(ViewerEvent.RequestQuote, callback);
   }
@@ -175,7 +111,7 @@ export class ElfsquadShowroom {
         const configurationId = json.argument;
         this.executeCallbacks(ViewerEvent.RequestQuote, { configurationId });
       } catch (e) {
-        // Ignore errors.
+        return;
       }
     });
   }
@@ -209,22 +145,14 @@ export class ElfsquadShowroom {
     return iframe;
   }
 
-  /**
-   * Determines the appropriate container for the iframe based on the provided options.
-   * @param options - The options used to initialize the showroom, including the container specification.
-   * @returns The resolved HTMLElement to use as the container.
-   * @throws Will throw an error if the specified container is not found or is not an HTMLElement.
-   */
   private getContainer(options: ElfsquadShowroomOptions): HTMLElement {
     if (typeof options.container === 'string') {
-      const element = document.querySelector(options.container);
-      if (!element) {
+      const ret = document.querySelector(options.container);
+      if (ret === null)
         throw new Error('Container not found');
-      }
-      if (!(element instanceof HTMLElement)) {
+      if (!(ret instanceof HTMLElement))
         throw new Error('Container must be an HTMLElement');
-      }
-      return element;
+      return ret;
     }
     return options.container;
   }
